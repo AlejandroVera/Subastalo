c76e5668bd6547849ddf82d73ea64a50aa76ef4e
kmilinho <c.pereira.leon@gmail.com>
2013-11-06 18:46:52 +0000

    empty commit message

---
+++ js/inicio.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+	if (window.location.href.split("?")[1] =="cerrando"){
+		parent.putLoginButton();
+	}
+
+}); 
--- logout.php
+++ logout.php
@@ -6,7 +6,7 @@ define('NEEDED_ACCESS_LEVEL', 0);
 //Cargamos el core
 require (IS2_ROOT_PATH . "core.php");
 logout();
-header('Location: inicio.php'); 
+header('Location: inicio.php?cerrando'); 
 
 /**
  * Vacia la sesion con los datos del usuario validado
