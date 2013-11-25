347fa727ce732d243be8476733685c0b28b498a3
AlejandroVera <xafilox@gmail.com>
2013-11-25 18:07:59 +0000

    Página de inicio

--- inicio.php
+++ inicio.php
@@ -6,6 +6,41 @@ define('NEEDED_ACCESS_LEVEL', 0);
 //Load core
 require (IS2_ROOT_PATH . "core.php");
 
+
+$random = mysqli_fetch_all(doquery("SELECT * FROM {{table}} ORDER BY RAND() LIMIT 6;", "subastas"), MYSQLI_ASSOC); //No es eficiente pero sirve
+$acabanAntes = mysqli_fetch_all(doquery("SELECT * FROM {{table}} WHERE comienzo+duracion > UNIX_TIMESTAMP() ORDER BY comienzo+duracion ASC LIMIT 6;", "subastas"), MYSQLI_ASSOC);
+$nuevas = mysqli_fetch_all(doquery("SELECT * FROM {{table}} WHERE comienzo+duracion > UNIX_TIMESTAMP() ORDER BY comienzo DESC LIMIT 6;", "subastas"), MYSQLI_ASSOC);
+
+for($x = 0; $x < count($random); $x++){
+    $texto = $random[$x]["imagen"];
+    if($texto != ""){
+        $imagenes = explode("|",$texto);
+        $random[$x]["imagen"] = $imagenes[0];
+    }
+}
+
+for($x = 0; $x < count($acabanAntes); $x++){
+    $texto = $acabanAntes[$x]["imagen"];
+    if($texto != ""){
+        $imagenes = explode("|",$texto);
+        $acabanAntes[$x]["imagen"] = $imagenes[0];
+    }
+}
+
+for($x = 0; $x < count($nuevas); $x++){
+    $texto = $nuevas[$x]["imagen"];
+    if($texto != ""){
+        $imagenes = explode("|",$texto);
+        $nuevas[$x]["imagen"] = $imagenes[0];
+    }
+}
+
+$smarty -> assign('random', $random);
+$smarty -> assign('acabanAntes', $acabanAntes);
+$smarty -> assign('nuevas', $nuevas);
+
+$smarty -> assign('scripts', array("inicio.js", "jquery.nivo.slider.pack.js"));
+$smarty -> assign('css', array("nivo-slider.css"));
 $smarty -> assign('nivelAcceso', estoy_logeado());
 $smarty -> assign('nombreUsuario', userName());
 $smarty -> assign('USUARIO_LOGUEADO', userId());
--- smarty-data/templates/inicio.tpl
+++ smarty-data/templates/inicio.tpl
@@ -3,5 +3,50 @@
 {include file ="barra.tpl" nivelAcceso=$nivelAcceso IN_ADMIN=$IN_ADMIN nombreUsuario=$nombreUsuario aceptaMsg=$aceptaMsg} 
 
 <div><p>Pagina principal</p></div>
+<div id="random">
+	<div class="slider-wrapper theme-default">
+		<div class="ribbon"></div>
+		<div id="slider" class="nivoSlider">
+			{foreach from=random item=elem}
+				{if !empty($elem['imagen']) }
+					<img src="images/uploaded/{$elem['imagen']}" alt="" data-transition="slideInLeft"/>
+				{else}
+					<img src="images/noImage.jpg">	
+				{/if}
+	   		{/foreach}
+		</div>
+	</div>
+</div>
+<div id="acabanAntes">
+	<div>A punto de terminar</div>
+	<div>
+	{foreach from=$acabanAntes item=elem}
+		<div class="resultadoInicio">
+			<div class="nombreResultado">{$elem['nombre']}</div>
+			{if !empty($elem['imagen']) }
+				<img class="imagenResultado" src={$elem['imagen']} />
+			{else}
+				<img src="images/noImage.jpg">	
+			{/if}
+			<img class="imagenResultado" src={$elem['imagen']} />
+		<div>
+	{/foreach}
+	</div>
+</div>
+<div id="nuevas">
+	<div>Nuevos productos en subasta</div>
+	<div>
+	{foreach from=$nuevas item=elem}
+		<div class="resultadoInicio">
+			<div class="nombreResultado">{$elem['nombre']}</div>
+			{if !empty($elem['imagen']) }
+				<img class="imagenResultado" src={$elem['imagen']} />
+			{else}
+				<img src="images/noImage.jpg">	
+			{/if}
+		<div>
+	{/foreach}
+	</div>
+</div>
 
 {include file="footer.tpl"}
